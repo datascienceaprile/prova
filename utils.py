7640212f351de06b09b9bd68dd27029a90257764
giulio <giulio.galvan@gmail.com>
2021-05-14 12:52:43 +0200

    funzioni utilità immagini

---
+++ utils.py
@@ -0,0 +1,32 @@
+ 
+def zoom(image: np.ndarray, factor: float):
+    """
+    resize keeping aspect ratio
+    """
+    height, width = image.shape[:2]
+    image = Image.fromarray(image)
+    resized = image.resize((int(width * factor), int(height * factor)), resample=Image.NEAREST)
+    return np.array(resized)
+
+
+
+def pad(image,
+        offset_bottom,
+        offset_top,
+        offset_right,
+        offset_left,
+        fillwith):
+    """
+    image    : image to pad
+    offset_bottom/top/left/right: pixels to pad on the bottom/top/left/right
+    fillwith : pixel value to use when padding
+    """
+    if len(image.shape) == 2: image = image[:, :, np.newaxis]
+    padded = np.pad(image,
+                    ((offset_top, offset_bottom), (offset_left, offset_right), (0, 0)),
+                    'constant',
+                    constant_values=fillwith)
+    if padded.shape[-1] == 1:
+        return padded.squeeze()
+    else:
+        return padded
